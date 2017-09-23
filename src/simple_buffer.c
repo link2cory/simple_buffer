f55f56f9557f0a9256dee9761a17e9218ed4ff2c
Cory <Link2cory@gmail.com>
2017-09-23 15:17:37 -0700

    added basic constructor skeleton

--- src/simple_buffer.c
+++ src/simple_buffer.c
@@ -1,1 +1,8 @@
 #include "simple_buffer.h"
+
+/*******************************************************************************
+* PUBLIC FUNCTION DEFINITIONS
+*******************************************************************************/
+char simple_buffer_construct(simple_buffer_attr_t *attr, sbd_t *sbd) {
+  return 0;
+}
--- src/simple_buffer.h
+++ src/simple_buffer.h
@@ -1,5 +1,18 @@
 #ifndef _SIMPLE_BUFFER_H
 #define _SIMPLE_BUFFER_H
 
+/*******************************************************************************
+* TYPEDEFS
+*******************************************************************************/
+typedef char sbd_t;
+typedef struct simple_buffer_attr_t {
+  char num_elem;
+  char (*buf_mem)[];
+} simple_buffer_attr_t;
+
+/*******************************************************************************
+* FUNCTION DECLARATIONS
+*******************************************************************************/
+char simple_buffer_construct(simple_buffer_attr_t *attr, sbd_t *sbd);
 
 #endif // _SIMPLE_BUFFER_H
